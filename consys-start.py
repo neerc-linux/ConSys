07955a50849cfc3ac0158e9a60188f036678cdfa
Kirill Elagin <kirelagin@gmail.com>
2011-04-13 23:07:37 +0400

    Fix Python 2 compatibility again

--- consys-start.py
+++ consys-start.py
@@ -9,7 +9,7 @@ valid_actions = ['client', 'server']
 
 def choose_by_name(name):
     for a in valid_actions:
-        if name[-len(a):] == a:
+        if name.endswith(a):
             return a
     return None
 
@@ -17,14 +17,17 @@ if __name__ == '__main__':
     action = choose_by_name(sys.argv[0])
     if not action:
         if len(sys.argv) < 2 or not sys.argv[1] in valid_actions:
-            print('Please specify one of {{{}}}.'.format(', '.join(valid_actions)), file=sys.stderr)
+            print('Please specify one of {{{0}}}.'.format(', '.join(valid_actions)),
+                  file=sys.stderr)
             sys.exit(-1)
         else:
             action = sys.argv[1]
             sys.argv = sys.argv[0:1] + sys.argv[2:]
     
     try:
-        a = __import__('consys.' + action, fromlist=['run'])
+        # FIXME when migrating to Python 3!
+        a = __import__('consys.' + action, fromlist=[b'run'])
         a.run()
     except ImportError as e:
-        print('Sorry, "{}" is not available on this system'.format(action), file=sys.stderr)
+        print('Sorry, "{}" is not available on this system'.format(action),
+              file=sys.stderr)
