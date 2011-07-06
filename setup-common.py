e94577795c7c74e702c7433139b24c6159e0f0e8
Nikita Ofitserov <himikof@gmail.com>
2011-07-06 17:59:32 +0400

    Add forgotten sub-package

--- setup-common.py
+++ setup-common.py
@@ -7,7 +7,7 @@ setup(
     version=version,
     url="http://gitorious.org/neerc-linux/consys",
     namespace_packages=['consys'],
-    packages=['consys.common'],
+    packages=['consys.common', 'consys.common.ampi'],
     author="NEERC Linux group",
     author_email="acm-neerc@googlegroups.com"
 )
