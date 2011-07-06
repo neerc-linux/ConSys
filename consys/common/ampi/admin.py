d22efa042e86aef8a0286821d92ce87326e84520
Nikita Ofitserov <himikof@gmail.com>
2011-07-06 23:39:04 +0400

    Fix AMP interface types

--- consys/common/ampi/admin.py
+++ consys/common/ampi/admin.py
@@ -20,16 +20,16 @@ class Ping(amp.Command):
 
 class GetTerminals(amp.Command):
     arguments = []
-    response = [(b'ids', amp.ListOf(amp.Integer))]
+    response = [(b'ids', amp.ListOf(amp.Integer()))]
     
 class GetTerminalData(amp.Command):
-    arguments = [(b'id', amp.Integer)]
-    response = [(b'name', amp.String),
-                (b'online', amp.Boolean)]
+    arguments = [(b'id', amp.Integer())]
+    response = [(b'name', amp.String()),
+                (b'online', amp.Boolean())]
     errors = {NoSuchObjectError: "NO_SUCH_OBJECT"}
 
 class ShutdownTerminal(amp.Command):
-    arguments = [(b'id', amp.Integer)]
+    arguments = [(b'id', amp.Integer())]
     response = []
     errors = {NoSuchObjectError: "NO_SUCH_OBJECT",
               TerminalOfflineError: "TERMINAL_OFFLINE"}
@@ -37,17 +37,17 @@ class ShutdownTerminal(amp.Command):
 # Server -> Admin commands
 
 class NewTerminal(amp.Command):
-    arguments = [(b'id', amp.Integer)]
+    arguments = [(b'id', amp.Integer())]
     response = []
     requiresAnswer = False
     
 class RemovedTerminal(amp.Command):
-    arguments = [(b'id', amp.Integer)]
+    arguments = [(b'id', amp.Integer())]
     response = []
     requiresAnswer = False
     
 class TerminalStatusUpdated(amp.Command):
-    arguments = [(b'id', amp.Integer), (b'online', amp.Boolean)]
+    arguments = [(b'id', amp.Integer()), (b'online', amp.Boolean())]
     response = []
     requiresAnswer = False
 
